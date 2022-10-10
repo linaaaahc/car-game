982fd2026edfc7f3b0ee3a607f43548950b5bb99
guessing-game <linaaaah420@gmail.com>
2022-10-10 10:22:16 +0100

    initial failed attempt

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+initial_command = input("> ")
+
+while initial_command:
+    if initial_command = "help"
+        print('''Start - to start the car
+            Stop - stop the car
+            Quit - to exit''')
 
